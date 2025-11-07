5e8a4982bae50ebe30f1675e368e019864242c07
Almagribii <brucadalm@gmail.com>
2025-11-08 06:03:01 +0700

    update form create and edit with formAction

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -10,7 +10,9 @@ import {
 import Link from "next/link";
 import { Button } from "@/app/ui/button";
 import { UpdateInvoice } from "./buttons";
-import { updateInvoice } from "@/app/lib/actions";
+import { updateInvoice, State } from "@/app/lib/actions";
+import { useActionState } from "react";
+
 
 export default function EditInvoiceForm({
   invoice,
@@ -19,10 +21,14 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
-  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const id = invoice.id;
+  const updateInvoiceWithId = updateInvoice.bind(null, id);
+  const initialState: State = { message: null, errors: {} };
+    const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
+  
 
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
